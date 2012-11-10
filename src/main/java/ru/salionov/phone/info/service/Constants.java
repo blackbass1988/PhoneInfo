bb26fc0190a58cfd6e90b1af2656c54e283f5ec1
Oleg Salionov <o.salionov@zmeke.com>
2012-11-10 16:21:33 +1100

    constants updated

---
+++ src/main/java/ru/salionov/phone/info/service/Constants.java
@@ -0,0 +1,12 @@
+package ru.salionov.phone.info.service;
+
+/**
+ * @author blackbass <o.salionov@zmeke.com>
+ */
+public class Constants {
+    public static final String HOST = "http://www.gsmarena.com/";
+    public static final int TIMEOUT = 10000;
+    public static final boolean FOLLOW_REDIRECTS = true;
+    public static final String USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.7; rv:10.0.2) Gecko/20100101 Firefox/10.0.2";
+
+}
